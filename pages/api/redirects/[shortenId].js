f55fa78ff20256470f3b837795084b803c0268e9
Coding Bugs <coding.bugs@outllok.com>
2021-03-06 23:39:10 +0100

    feat(api): add model repository for getting the right url and do the redirection

--- pages/api/redirects/[shortenId].js
+++ pages/api/redirects/[shortenId].js
@@ -1,7 +1,23 @@
-export default (req, res) => {
-    const shortenId = req.query.shortenId;
+import { Model } from '../../../services/Model.js';
+import Shortened from '../../../services/Shortened.js';
 
-    res.statusCode = 200;
-    res.json({ id: shortenId });
+
+export default (req, res) => {
+  
+  let service = new Shortened({
+    domain: 'localhost',
+    model: Model,
+    protocol: 'http',
+    port: 3000
+  });
+
+  const id = req.query.shortenId;
+  const item = service.get(id);
+
+  if(null === item) {
+      res.status(404).json({ error: 'not found' });
+  } else {
+      res.redirect(307, item.source);
   }
+}
   
