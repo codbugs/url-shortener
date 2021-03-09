7af3a305c379bcdc6681bf7c387c75cccf4e9993
Coding Bugs <coding.bugs@outllok.com>
2021-03-09 20:14:55 +0100

    feat(app): add ul tag

--- components/ShortenedItemsList.js
+++ components/ShortenedItemsList.js
@@ -1,11 +1,14 @@
 import ShortenedItemRow from './ShortenedItemRow.js';
 
 export default function ShortenedItemsList(props) {
-    return props.items && props.items.map(
-        (item, index) => {
-            return <li key={index}>
-                <ShortenedItemRow item={item} />
-            </li>;
-        }
-    );
+    return <ul>
+        {
+            props.items && props.items.map(
+                (item, index) => {
+                    return <li key={index}>
+                        <ShortenedItemRow item={item} />
+                    </li>;
+                })
+        }  
+    </ul>;
 }
