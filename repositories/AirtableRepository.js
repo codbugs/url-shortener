2a7a49bad91b4c4578f929e379a0de83db9a3add
Coding Bugs <coding.bugs@outlook.com>
2021-03-29 00:23:46 +0200

    feat(app): add object to manage calls to Airtable cloud service

---
+++ repositories/AirtableRepository.js
@@ -0,0 +1,50 @@
+export default function AirtableRepository({ table }) {
+
+    const authId = process.env.AIRTABLE_API_KEY;
+    const baseUrl = process.env.AIRTABLE_API_BASE_URL;
+    const apiVersion = process.env.AIRTABLE_API_VERSION;
+    const baseId = process.env.AIRTABLE_BASE_ID;
+
+    const endpoint = `${baseUrl}/v${apiVersion}/${baseId}/${table}`;
+
+    const request = ({ method, token, body }) => {
+        return fetch(endpoint, {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: undefined !== body ? JSON.stringify(body): undefined
+        })
+        .then(response => response.json());
+    };
+
+    return {
+        get() {
+            return request({
+                method: 'GET',
+                token: authId
+            })
+        },
+
+        patch(collection) {
+            return request({
+                method: 'PATCH',
+                token: authId,
+                body: {
+                    records: collection
+                }
+            });
+        },
+
+        post(item) {
+            return request({
+                method: 'POST',
+                token: authId,
+                body: {
+                    records: [item]
+                }
+            });
+        },
+    }
+}
--- repositories/Analytics.js
+++ repositories/Analytics.js
@@ -1,50 +1,36 @@
-export default function AnalyticsRepository() {
+import AirtableRepository from './AirtableRepository.js';
 
-    const AIRTABLE_API_KEY = 'keySwLy8GeOaJ9y1q';
-    const BASE_ID = 'app09VWldHzsDVa11';
-    const LINKS_TABLE_NAME = 'Analytics';
-    const AIRTABLE_API_BASE_URL = 'https://api.airtable.com';
-    const AIRTABLE_API_VERSION = '0';
-    const url = `${AIRTABLE_API_BASE_URL}/v${AIRTABLE_API_VERSION}/${BASE_ID}/${LINKS_TABLE_NAME}`;
+
+export default function AnalyticsRepository() {
+    
+    const master = new AirtableRepository({ table: 'Analytics' });
 
     function transform(response) {
         return response.records.map(record => {
-                    return {
-                        id: record.id,
-                        resourceId: record.fields.resourceId,
-                        headers: record.fields.headers,
-                        sourceUrl: record.fields.sourceUrl,
-                        ip: record.fields.ip,
-                        created: record.fields.created
-                    };
-                });
+            return {
+                id: record.id,
+                resourceId: record.fields.resourceId,
+                headers: record.fields.headers,
+                sourceUrl: record.fields.sourceUrl,
+                ip: record.fields.ip,
+                created: record.fields.created
+            };
+        });
     }
-
 
     return {
         create({ resourceId, headers, sourceUrl, ip, created }) {
-            return fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${AIRTABLE_API_KEY}`
-                },
-                body: JSON.stringify({
-                    records: [
-                        {
-                            fields: {
-                                resourceId,
-                                headers,
-                                sourceUrl,
-                                ip,
-                                created
-                            }
-                        }
-                    ]
-                })
-            })
-            .then(response => response.json())
-            .then(transform);
+            const item = {
+                fields: {
+                    resourceId,
+                    headers,
+                    sourceUrl,
+                    ip,
+                    created
+                }
+            };
+
+            return master.post(item).then(transform);
         }
     };
 }
