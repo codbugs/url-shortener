51122a2c6e0269f33f45d1e2ae7ce4f5c4bc9fe2
Coding Bugs <coding.bugs@outlook.com>
2021-03-30 01:04:24 +0200

    feat(app): add link to home on links page

--- components/ShortenedItemAllItemsHeaderRow.js
+++ components/ShortenedItemAllItemsHeaderRow.js
@@ -5,5 +5,6 @@ import styles from './ShortenedItemAllItemsHeaderRow.module.css';
 export default function ShortenedItemAllItemsHeaderRow(props) {
     return <li className={styles.item}>
         <span className={styles.source}>All links</span>
+        <span className={styles.target}><Link href="/"><a>Back to Home</a></Link></span>
     </li>;
 }
